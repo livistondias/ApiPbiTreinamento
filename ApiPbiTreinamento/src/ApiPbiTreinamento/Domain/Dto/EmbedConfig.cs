945f1f89f0144d7ecc86718a4c9691732124a30e
livistondias <61847263+livistondias@users.noreply.github.com>
2022-08-17 20:33:42 -0300

    Add files via upload

---
+++ ApiPbiTreinamento/src/ApiPbiTreinamento/Domain/Dto/EmbedConfig.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.PowerBI.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPbiTreinamento.Domain.Dto
+{
+    public class EmbedConfig
+    {
+        public string Id { get; set; }
+
+        public string EmbedUrl { get; set; }
+
+        public string PageName { get; set; }
+
+        public EmbedToken EmbedToken { get; set; }
+
+        public int MinutesToExpiration
+        {
+            get
+            {
+                var minutesToExpiration = (DateTime)EmbedToken?.Expiration - DateTime.UtcNow;
+                return minutesToExpiration.Minutes;
+            }
+        }
+
+        public bool? IsEffectiveIdentityRolesRequired { get; set; }
+
+        public bool? IsEffectiveIdentityRequired { get; set; }
+
+        public bool EnableRLS { get; set; }
+
+        public string Username { get; set; }
+
+        public string Roles { get; set; }
+
+        public string ErrorMessage { get; set; }
+    }
+}
